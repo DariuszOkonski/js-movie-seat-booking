cead5749bfc098af0e15c07bb264f548d52a8f09
Dariusz Okonski <darek200180@gmail.com>
2020-05-02 13:38:06 +0100

    add methods pickAMovie, markSeat, checkNumberOfSelectedSeats, setTickets

--- src/js/View.js
+++ src/js/View.js
@@ -1,16 +1,44 @@
 class View {
     constructor() {
         this.selectedSeats = 0;
+        this.ticketFare = 10;
 
-        this.selectMovie = document.getElementById('select-movie');
-        
-        document.querySelectorAll('.seats .seat').forEach(seat => seat.addEventListener('click', this.markSeat));
-        document.querySelectorAll('.seats .seat.occupied').forEach(seat => seat.removeEventListener('click', this.markSeat));
+        this.selectMovie = document.getElementById('select-movie').addEventListener('change', this.pickAMovie.bind(this));
+        this.selectedSeatsNumber = document.getElementById('seats-number');
+        this.ticketPrice = document.getElementById('price');
+
+        document.querySelectorAll('.seats .seat').forEach(seat => seat.addEventListener('click', this.markSeat.bind(this)));     
     }
 
-    markSeat(){
-        this.classList.toggle('selected');
-        console.log(this);
+    pickAMovie(e) {
+        this.ticketFare = Number(e.target.options[e.target.options.selectedIndex].value);
+
+        this.setTickets();
+    }
+
+    markSeat(e) {
+        if(e.target.classList.contains('occupied'))
+            return;
+
+        e.target.classList.toggle('selected');
+
+        this.checkNumberOfSelectedSeats();
+        this.setTickets();
+    }
+
+    checkNumberOfSelectedSeats() {
+        this.selectedSeats = document.querySelectorAll('.seats .seat.selected').length;
+    }
+
+    setTickets() {
+        console.log('set tickets');
+        this.selectedSeatsNumber.textContent = this.selectedSeats;
+        this.ticketPrice.textContent = this.selectedSeats * this.ticketFare;
+    }
+
+    randomlyOccupiedSeats() {
+        console.log('randomlyOccupiedSeat');
+        
     }
 }
 
