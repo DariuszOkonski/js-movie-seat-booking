eb6b8ad7d5f001a4cf69a0521283191911d38ad9
Dariusz Okonski <darek200180@gmail.com>
2020-05-02 13:50:00 +0100

    add randomlyOccupiedSeats

--- src/js/View.js
+++ src/js/View.js
@@ -7,12 +7,13 @@ class View {
         this.selectedSeatsNumber = document.getElementById('seats-number');
         this.ticketPrice = document.getElementById('price');
 
-        document.querySelectorAll('.seats .seat').forEach(seat => seat.addEventListener('click', this.markSeat.bind(this)));     
+        document.querySelectorAll('.seats .seat').forEach(seat => seat.addEventListener('click', this.markSeat.bind(this)));   
+        
+        this.randomlyOccupiedSeats();
     }
 
     pickAMovie(e) {
         this.ticketFare = Number(e.target.options[e.target.options.selectedIndex].value);
-
         this.setTickets();
     }
 
@@ -31,14 +32,21 @@ class View {
     }
 
     setTickets() {
-        console.log('set tickets');
         this.selectedSeatsNumber.textContent = this.selectedSeats;
         this.ticketPrice.textContent = this.selectedSeats * this.ticketFare;
     }
 
     randomlyOccupiedSeats() {
-        console.log('randomlyOccupiedSeat');
+        const occupiedSeats = Math.floor((Math.random() * 20) + 10);
+        const seatsToOccupied = [];
         
+        for(let i = 0; i < occupiedSeats; i++) {
+            seatsToOccupied.push(Math.floor(Math.random() * document.querySelectorAll('.seats .seat').length));
+        }
+
+        seatsToOccupied.forEach(seat => {
+            document.querySelectorAll('.seats .seat')[seat].classList.add('occupied')
+        });
     }
 }
 
